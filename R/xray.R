d5453499ddc9836951bd28cc41f732b806e06b6d
Brian Vancil <Brian@Vancil.name>
2020-01-15 19:53:36 -0600

    Unworking work in progress
    Tried to capture most of the relevant variables with the exception of input_col_names

--- R/xray.R
+++ R/xray.R
@@ -12,11 +12,22 @@
 xray <- function(.data, ..., .log = NULL) {
   # TODO: Add variables used in count as well
   # TODO: Figure out what to do with .log
-  process <- .data %>%
-    dplyr::count(...)
+  results <- dplyr::mutate(.data, ...)
+  # Does this not exist anymore? Newly added, but it doesn't resurn what I expected.
+  cols <- dplyr:::mutate_cols(.data, ...)
+  input_col_names <- base::setdiff(c(), base::names(cols$add)) # TODO: Figure out how to parse
+  truly_new_col_names <- base::setdiff(base::names(cols$add), base::names(.data))
+  modified_col_names <- base::intersect(base::names(cols$add), base::names(.data))
+  x_data <- .data[c(input_col_names, modified_col_names)]
+  base::names(x_data) <- stringr::str_c('X_', base::names(x_data))
+  y_data <- results[c(modified_col_names, truly_new_col_names)]
+  base::names(y_data) <- stringr::str_c('Y_', base::names(y_data))
+  xy_data <- base::cbind(x_data, y_data)
+  process <- xy_data %>%
+    dplyr::group_by_all() %>%
+    dplyr::tally() %>%
+    dplyr::ungroup()
   print(process)
 
-  .data %>%
-    dplyr::mutate(...) %>%
-    base::invisible()
+  base::invisible(results)
 }
--- dev/1_setup_dev_environment.R
+++ dev/1_setup_dev_environment.R
@@ -9,3 +9,10 @@ utils::install.packages(c(
   'tidyverse',
   'usethis'
 ))
+
+remotes::install_github('r-lib/vctrs')
+remotes::install_github('r-lib/pillar')
+remotes::install_github('tidyverse/tibble')
+remotes::install_github('r-lib/tidyselect')
+remotes::install_github('r-lib/rlang')
+remotes::install_github('tidyverse/dplyr')
